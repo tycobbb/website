ceb980c1763dc384ebae43140588e7dd647bb071
ty cobb <bin@tycobb.club>
2022-02-17 11:05:18 -0500

    make os a class

--- src/os/os.js
+++ src/os/os.js
@@ -12,175 +12,228 @@ const kClass = {
   IsInteracting: "is-interacting"
 }
 
-// -- props --
-/// the current location
-let mUrl = null
-
-/// the page container
-let $mPage = null
-
-/// the persistent container
-let $mPeristent = null
-
-// -- lifetime --
-function main() {
-  // set props
-  mUrl = document.location
-  $mPage = document.getElementById(kId.Page)
-  $mPeristent = document.getElementById(kId.Persistent)
-
-  // bind events
-  const d = document
-  d.addEventListener("click", didClick)
-
-  const w = window
-  w.addEventListener("popstate", didPopState)
-
-  const p = $mPeristent
-  p.addEventListener(Frame.Events.GestureStart, didStartGesture)
-  p.addEventListener(Frame.Events.GestureEnd, didEndGesture)
-
-  // run post visit events first time
-  didFinishVisit()
+/// an enum of visit types
+const kVisit = {
+  None: 0,
+  SameOrigin: 1,
+  SamePath: 2
 }
 
-// -- commands --
-/// navigate to the url
-function navigate(url) {
-  // add history entry
-  history.pushState({}, "", url)
+/// the os
+class Os {
+  // -- props --
+  /// the current location
+  url = null
 
-  // visit page
-  visit(url)
-}
+  // -- p/el
+  /// the page container
+  $page = null
 
-/// visit the url and update the game
-async function visit(url) {
-  // run pre visit events
-  didStartVisit()
+  /// the persistent container
+  $peristent = null
 
-  // update the browser url
-  mUrl = url
+  // -- lifetime --
+  /// create a new os
+  constructor() {
+    const m = this
 
-  // download the page
-  const resp = await fetch(url)
-  const text = await resp.text()
-
-  // render the element
-  const $el = document.createElement("html")
-  $el.innerHTML = text
-
-  // extract the page
-  const $next = $el.querySelector(`#${kPageId}`)
-
-  // replace children of page element
-  while ($mPage.firstChild) {
-    $mPage.removeChild($mPage.lastChild)
+    // set props
+    m.url = document.location
+    m.$page = document.getElementById(kId.Page)
+    m.$peristent = document.getElementById(kId.Persistent)
   }
 
-  for (const child of Array.from($next.children)) {
-    $mPage.appendChild(child)
+  // -- commands --
+  /// start the os
+  start() {
+    const m = this
+
+    // bind events
+    const d = document
+    d.addEventListener("click", m.didClick)
+
+    const w = window
+    w.addEventListener("popstate", m.didPopState)
+
+    const p = m.$peristent
+    p.addEventListener(Frame.Events.GestureStart, m.didStartGesture)
+    p.addEventListener(Frame.Events.GestureEnd, m.didEndGesture)
+
+    // run post visit events first time
+    m.didFinishVisit()
   }
 
-  // TODO: do we need this?
-  // activate any inert script tags in the new game
-  const scripts = $mPage.querySelectorAll("script")
-  for (const inert of Array.from(scripts)) {
-    // clone the inert script tag
-    const script = document.createElement("script")
-    script.textContent = inert.textContent
-    for (const { name, value } of inert.attributes) {
-      script.setAttribute(name, value)
+  /// navigate to the url
+  navigate(url) {
+    const m = this
+
+    // add history entry
+    history.pushState({}, "", url)
+
+    // visit page
+    m.visit(url)
+  }
+
+  /// visit the url and update the game
+  async visit(url) {
+    const m = this
+
+    // run pre visit events
+    m.didStartVisit()
+
+    // update the browser url
+    m.url = url
+
+    // download the page
+    const resp = await fetch(url)
+    const text = await resp.text()
+
+    // render the element
+    const $el = document.createElement("html")
+    $el.innerHTML = text
+
+    // extract the page
+    const $next = $el.querySelector(`#${kId.Page}`)
+
+    // replace children of page element
+    while (m.$page.firstChild) {
+      m.$page.removeChild(m.$page.lastChild)
     }
 
-    // and replace it with the active one
-    const parent = inert.parentElement
-    parent.replaceChild(script, inert)
+    for (const child of Array.from($next.children)) {
+      m.$page.appendChild(child)
+    }
+
+    // TODO: do we need this?
+    // activate any inert script tags on the new page
+    const scripts = m.$page.querySelectorAll("script")
+    for (const inert of Array.from(scripts)) {
+      // clone the inert script tag
+      const script = document.createElement("script")
+      script.textContent = inert.textContent
+      for (const { name, value } of inert.attributes) {
+        script.setAttribute(name, value)
+      }
+
+      // and replace it with the active one
+      const parent = inert.parentElement
+      parent.replaceChild(script, inert)
+    }
+
+    // run post visit events
+    m.didFinishVisit()
   }
 
-  // run post visit events
-  didFinishVisit()
-}
+  // -- queries --
+  /// get the visit type for a change to this url
+  getVisit(url) {
+    const m = this
 
-// -- queries --
-/// if the url should fire a visit
-function shouldStartVisit(url) {
-  // if the paths aren't the same (a hashchange may trigger popstate, but
-  // we don't want to re-render)
-  return (
-    mUrl.origin === url.origin &&
-    mUrl.pathname !== url.pathname
-  )
-}
+    // default to no visit, browser nav
+    let type = kVisit.None
 
-// -- events --
-/// when anything is clicked on
-function didClick(evt) {
-  // see if there is an enclosing link
-  let $t = evt.target
-  while ($t != null && $t.tagName.toLowerCase() !== "a") {
-    $t = $t.parentElement
+    // if the origin matches
+    if (m.url.origin === url.origin) {
+      type = kVisit.SameOrigin
+
+      // if the path matches
+      if (m.url.pathname === url.pathname) {
+        type = kVisit.SamePath
+      }
+    }
+
+    return type
   }
 
-  // if we found one
-  if ($t == null) {
-    return
+  // -- events --
+  /// when anything is clicked on
+  didClick = (evt) => {
+    const m = this
+
+    // see if there is an enclosing link
+    let $t = evt.target
+    while ($t != null && $t.tagName.toLowerCase() !== "a") {
+      $t = $t.parentElement
+    }
+
+    // if, we didn't find a link, ignore
+    if ($t == null) {
+      return
+    }
+
+    // if it has a target (like "_blank"), ignore
+    if ($t.getAttribute("target")) {
+      return
+    }
+
+    // grab its url (an svg link's href is an object)
+    let href = $t.href
+    if (typeof href === "object") {
+      href = href.baseVal.toString()
+    }
+
+    // if it has no url, ignore
+    if (!href) {
+      return
+    }
+
+    // get the visit type
+    const url = new URL(href, m.url)
+    const visit = m.getVisit(url)
+
+    // if none, ignore
+    if (visit === kVisit.None) {
+      return
+    }
+
+    // if some, cancel the click
+    evt.preventDefault()
+
+    // if not same path, run the visit
+    if (visit != kVisit.SamePath) {
+      m.navigate(url)
+    }
   }
 
-  // grab its url (an svg link's href is an object)
-  let href = $t.href
-  if (typeof href === "object") {
-    href = href.baseVal.toString()
+  /// when back is clicked
+  didPopState = () => {
+    const m = this
+
+    // get the visit for this url
+    const url = new URL(document.location.href)
+    const visit = m.getVisit(url)
+
+    // if none, do what the browser wants
+    if (visit === kVisit.None) {
+      return
+    }
+
+    // otherwise, visit the url
+    m.visit(url)
   }
 
-  // if we found one
-  if (!href) {
-    return
+  /// when a visit starts
+  didStartVisit() {
   }
 
-  // if we should visit this url
-  const url = new URL(href, mUrl)
-  if (!shouldStartVisit(url)) {
-    return
+  /// when a visit finishes
+  didFinishVisit() {
   }
 
-  // perform an in-page visit instead of the browser default
-  evt.preventDefault()
-
-  // navigate to the page
-  navigate(url)
-}
-
-/// when back is clicked
-function didPopState() {
-  const url = new URL(document.location.href)
-  if (!shouldStartVisit(url)) {
-    return
+  /// when a gesture starts
+  didStartGesture = () => {
+    this.$peristent.classList.toggle(kClass.IsInteracting, true)
   }
 
-  visit(url)
+  /// when a gesture ends
+  didEndGesture = () => {
+    this.$peristent.classList.toggle(kClass.IsInteracting, false)
+  }
 }
 
-/// when a visit starts
-function didStartVisit() {
-}
-
-/// when a visit finishes
-function didFinishVisit() {
-}
-
-/// when a gesture starts
-function didStartGesture() {
-  $mPeristent.classList.toggle(kClass.IsInteracting, true)
-}
-
-/// when a gesture ends
-function didEndGesture() {
-  $mPeristent.classList.toggle(kClass.IsInteracting, false)
-}
-
-// -- exports --
-window.navigate = navigate
+// -- singleton --
+const os = new Os()
+window.os = os
 
 // -- bootstrap --
-main()
+os.start()
